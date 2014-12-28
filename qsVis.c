5c79c950fd56ebab4fbf2ebd7d81c7595aac8712
Vivek Mehra <vivek.1376@gmail.com>
2014-12-28 18:38:37 +0530

    creating bars with propotionally scaled height

--- qsVis.c
+++ qsVis.c
@@ -5,10 +5,32 @@
 #define FL_COLOR(x) ((float)(x)/255.0f)
 #endif
 
-#define BASELINE_Y -0.05f
+#define BASELINE_Y -0.4f
 
 GLfloat angle=0.0;
 GLfloat pos=0.0;
+
+void spin(void);
+void display(void);
+void keyboard(unsigned char key, int x, int y);
+void reshape(int w, int h);
+void createBars(int *, int num);
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitWindowSize(600,400);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow ("sort visualisation");
+    glutDisplayFunc (display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc (keyboard);
+    //glutIdleFunc(spin);
+    glutMainLoop ();
+    return 0;
+}
+
 void spin(void)
 {
     printf("spin()\n");
@@ -29,9 +51,10 @@ void display(void)
 //    glutWireCube(2.0);
 
     glColor3f(0.2,0.5,0.8);
-
-    glRectf(0.005f,BASELINE_Y,0.03f,0.9f);
-    glRectf(0.035f,BASELINE_Y,0.06f,0.9f);
+    int num[]={10,4,6,20,10,7,2,14,9,4,5,20,11,2,7,13,1,3,19,4};
+    createBars(num,20);
+//    glRectf(0.005f,BASELINE_Y,0.03f,0.9f);
+    //  glRectf(0.035f,BASELINE_Y,0.06f,0.9f);
 
 //    glRectf(0.0f,0.0f,1.0f,-1.0f); //d
 
@@ -59,17 +82,18 @@ void reshape(int w, int h)
     glMatrixMode (GL_MODELVIEW);
 }
 
-int main(int argc, char *argv[])
+void createBars(int *values, int count)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE);
-    glutInitWindowSize(600,400);
-    glutInitWindowPosition(100,100);
-    glutCreateWindow ("sort visualisation");
-    glutDisplayFunc (display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc (keyboard);
-    //glutIdleFunc(spin);
-    glutMainLoop ();
-    return 0;
+    int i,max=0;
+    float xOffset=((float)count*0.03f)/2.0f;
+
+    /* find max value to find y scale length */
+    for(i=0;i<count;i++)
+	if(values[i]>max)
+	    max=values[i];
+
+    for(i=0;i<count;i++)
+    {
+	glRectf(i*0.03f-xOffset+0.005f,BASELINE_Y,(i+1)*0.03f-xOffset,((float)values[i]/max)*(0.9f-BASELINE_Y)+BASELINE_Y);	
+    }
 }
